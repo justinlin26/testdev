632819bac9c6fa840725d22422ddbc6fb94970df
Justin <justin77lin@gmail.com>
2020-05-22 20:11:38 -0700

    test

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var app = express();
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -17,7 +25,22 @@ app.get('/', function(req, res) {
     // ejs render automatically looks in the views folder
     res.render('index');
 });
-
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+app.get("/cool", function(req,res){
+    //ejs redner automatically looks in the views folder
+    res.render('aboutus.ejs');
+});
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
