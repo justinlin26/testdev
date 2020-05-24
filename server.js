6fa33ff7664a69f6c4261b96b8bd24d94b1caf92
Justin <justin77lin@gmail.com>
2020-05-23 17:28:52 -0700

    database

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const pool = new Pool({
 app.get('/db', async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM test_table');
+    const result = await client.query('SELECT * FROM test1');
     const results = { 'results': (result) ? result.rows : null};
     res.render('pages/db', results );
     client.release();
