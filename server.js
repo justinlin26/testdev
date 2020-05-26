62c77af5f1887b919bbdd9d8f4983659a3d70c3f
Justin <justin77lin@gmail.com>
2020-05-26 11:53:00 -0700

    test

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/search', async (req, res) => {
     
     const results = { 'results': (result) ? result.rows : null};
     
-    console.log(results);
+    console.log(search);
     res.render('db', results );
     client.release();
   } catch (err) {
