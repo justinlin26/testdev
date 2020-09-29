0c71c24e3dc3a33707c140cc8f31e5544a68e978
Justin <justin77lin@gmail.com>
2020-09-29 15:31:52 -0700

    test

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var cloudinary = require('cloudinary');
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
@@ -75,6 +78,12 @@ app.post('/create',async(req,res)=>{
     const results = { 'results': (result) ? result.rows : null};
     
     console.log("results sumbitted!!!!!!!!!!!!!!!"+results);
+    stream = cloudinary.uploader.upload_stream(function(result) {
+      console.log(result);
+      res.send('Done:<br/> <img src="' + result.url + '"/><br/>' +
+               cloudinary.image(result.public_id, { format: "png", width: 100, height: 130, crop: "fill" }));
+    }, { public_id: req.body.title } );
+    fs.createReadStream(req.files.image.path, {encoding: 'binary'}).on('data', stream.write).on('end', stream.end);
     client.release();
     res.redirect('/');
   } catch (err) {
